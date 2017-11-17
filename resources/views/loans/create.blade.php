f93aecbd10f47d3c0a4bfbe17d6bd89545cd4461
Gerson <grodriguez@el.cr>
2017-11-16 22:26:11 -0600

    added new interest calculation

--- resources/views/loans/create.blade.php
+++ resources/views/loans/create.blade.php
@@ -1,123 +1,138 @@
 @extends('layouts.app')
 
 @section('styles')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/nouislider.css') }}">
+<style type="text/css">
+label
+{
+  font-weight: lighter !important;
+  text-align: center;
+  color: #585858;
+  width: 100%;
+}
+</style>
 @endsection
 
 @section('content')
-<div class="container pb-5">
-
+<div class="container py-3">
   <div class="row">
-    <div class="col-12 ">
-      <br><h5 class="page-header text-center">1. Definir el monto</h5><br>
+    <div class="col-md-6 pb-3">
+      <label class="page-header text-center">Monto a Prestar</label>
       <div class="form-row justify-content-center">
-        <div class="col-9 col-md-6 big-input">
+        <div class="col-9 big-input">
           <div class="input-group">
             <span class="input-group-addon">₡</span>
-            <input id="input_amount" type="text" pattern="\d*" class="form-control amount-control py-3" placeholder="Monto" value="{{old('loan')}}" required>
+            <input id="input_loan_amount" type="text" pattern="\d*" class="form-control amount-control py-3" placeholder="Monto" value="{{old('loan')}}" required>
           </div>
         </div>
       </div>{{-- form row --}}
-    </div>{{-- col-12 --}}
-  </div>{{-- row --}}
-
-  <hr>
-
-  <div class="row">
-    <div class="col-12 ">
-      <br><h5 class="page-header text-center">2. Escoger la tasa de interés</h5><br>
+    </div>{{-- col-6 --}}
+    <div class="col-md-6 pb-3">
+      <label class="page-header text-center">Monto a Pagar</label>
       <div class="form-row justify-content-center">
-        <div class="col-9 col-md-6 py-3">
-          <div id="interest_slider"></div>
-          <h4 class="text-center pt-4"><span id="label_interests">20</span>% de interés</h4>
-          <h5 class="text-center pt-2"><small>₡<span id="label_loan_amount">0</span> total.</small></h5>
+        <div class="col-9 big-input">
+          <div class="input-group">
+            <span class="input-group-addon">₡</span>
+            <input id="input_balance_amount" type="text" pattern="\d*" class="form-control amount-control py-3" placeholder="Saldo" value="{{old('balance')}}" required>
+          </div>          
         </div>
       </div>{{-- form row --}}
-    </div>{{-- col-12 --}}
+    </div>{{-- col-6 --}}
   </div>{{-- row --}}
-
+  
   <hr>
 
   <div class="row">
-    <div class="col-12 ">
-      <br><h5 class="page-header text-center">3. Configurar el plan de pago</h5><br>
+    <div class="col-md-6 text-center">
+      <h4 class="m-0 pt-2">
+        <span id="label_interests" class="font-weight-bold">25</span>% de interés
+      </h4>
+    </div><!-- /.col-md-6 -->
 
+    <div class="col-md-6">
+      <div class="row">
+        <div class="col-3 col-md-2">
+          <input id="input_duration" type="numer" class="form-control text-center m-0" name="duration" value="5" min="1" max="99" required>
+        </div>
+        <div class="col pt-2">
+          <div class="form-check form-check-inline m-0">
+            <label class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('we')" value="we" checked>
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Semanas</span>
+            </label>
+          </div>
+          <div class="form-check form-check-inline m-0">
+            <label class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('bw')" value="bw">
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Quincenas</span>
+            </label>
+          </div>
+          <div class="form-check form-check-inline m-0">
+            <label class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('mo')" value="mo">
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Meses</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.col-md-6 -->
+  </div><!-- /.row -->
+
+  <hr>
+
+  <div class="row pt-5">
+
+    <div class="col-md-6">
+      <label class="page-header text-center">Monto de las Cuotas</label>
       <div class="form-row justify-content-center">
-        <div class="col-9 col-md-6 py-3">
-          <div id="duration_slider"></div>
-          <h4 class="text-center pt-4">Duracion: <span id="label_duration">1</span> <span id="label_plan" >Semanas</span></h4>
+        <div class="col-9 big-input">
+          <div class="input-group">
+            <span class="input-group-addon">₡</span>
+            <input id="input_regdue_amount" style="background: transparent !important;" type="text" class="form-control amount-control py-3" placeholder="Cuotas" readonly>
+          </div>
         </div>
       </div>{{-- form row --}}
+    </div>{{-- col-6 --}}
 
+    <div class="col-md-6 ">
+      <label class="page-header text-center">Cuota Minima</label>
       <div class="form-row justify-content-center">
-        <div class="col-9 col-md-6 py-3">
-          <div class="card text-center">
-            <div class="card-header">Cuotas</div>
-            <div class="card-body">
-              <h3 class="card-title">₡<span id="dues_amount" >0</span></h3>
-              <p class="card-text mb-0">Abono: ₡<span id="dues_deposit">0</span> </p>
-              <p class="card-text">Intereses: ₡<span id="dues_minimum">0</span> </p>
-            </div>{{-- body --}}
-            <div class="card-footer text-center pt-4">
-              <div class="form-check form-check-inline">
-                <label class="custom-control custom-radio">
-                  <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('we')" value="we" checked>
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Semanal</span>
-                </label>
-              </div>
-              <div class="form-check form-check-inline">
-                <label class="custom-control custom-radio">
-                  <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('bw')" value="bw">
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Quincenal</span>
-                </label>
-              </div>
-              <div class="form-check form-check-inline">
-                <label class="custom-control custom-radio">
-                  <input class="custom-control-input" type="radio" name="type" onchange="onPayPlanChange('mo')" value="mo">
-                  <span class="custom-control-indicator"></span>
-                  <span class="custom-control-description">Mensual</span>
-                </label>
-              </div>
-            </div>
-          </div> {{-- card --}}
-        </div>{{-- col-3 --}}
+        <div class="col-9 big-input">
+          <div class="input-group">
+            <span class="input-group-addon">₡</span>
+            <input id="input_mindue_amount" type="text" pattern="\d*" class="form-control amount-control py-3" placeholder="Minimo" required>
+          </div>          
+        </div>
+        <label>+ <span id="label_extmin">0</span></label>
       </div>{{-- form row --}}
-
-    </div>{{-- col-12 --}}
+    </div>{{-- col-6 --}}
   </div>{{-- row --}}
 
-  <br>
   <hr>
-  <br>
 
-  <div class="row">
-
+  <div class="row pt-3">
     <div class="col-md-12">
       @if ( !isset( $_GET['auto'] ) )
-      <br><h5 class="page-header text-center">4. Agregar el Cliente</h5><br>
+      <label class="page-header pb-4">Agregar el Cliente</label>
       @endif
-
       <form id="newLoanForm" method="post" action="{{ route('loans.store') }}">
         {{ csrf_field() }}
-        <input id="loan" type="hidden" name="loan" value="0">
-        <input id="total" type="hidden" name="total" value="0">
-        <input id="interest" type="hidden" name="interest" value="20">
-        <input id="duration" type="hidden" name="duration" value="6">
+        <input id="loaned" type="hidden" name="loaned" value="0">
+        <input id="balance" type="hidden" name="balance" value="0">
+        <input id="intrate" type="hidden" name="intrate" value="0">
+        <input id="intval" type="hidden" name="intval" value="0">
+        <input id="duration" type="hidden" name="duration" value="5">
         <input id="payplan" type="hidden" name="payplan" value="we">
-        <input id="details" type="hidden" name="details" value="1">
-        <input id="dues" type="hidden" name="dues" value="0">
-        <input id="partial" type="hidden" name="partial" value="0">
-
-
+        <input id="regdue" type="hidden" name="regdue" value="0">
+        <input id="mindue" type="hidden" name="mindue" value="0">
         @if ( isset( $_GET['auto'] ) )
         <input type="hidden" name="first_name" value="0">
         <input type="hidden" name="last_name" value="0">
         <input type="hidden" name="phone" value="{{ $_GET['key'] }}">
         @else
         <div class="form-row">
-
           <div class="col-6 col-md-4 form-group">
             <input type="text"  name="first_name" class="form-control py-3 {{ $errors->has('first_name') ? 'is-invalid' : '' }}" value="{{ old('first_name') }}" placeholder="Nombre *" required>
             <div class="invalid-feedback {{ $errors->has('first_name') ? 'd-block' : 'd-none' }}">{{ $errors->first('first_name') }}</div>
@@ -142,7 +157,6 @@
             <input type="number" name="phone_work" class="form-control py-3" value="{{ old('phone_work') }}" placeholder="Telefono (trabajo)">
           </div>
 
-
           <div class="col-md-6 form-group mt-3">
             <textarea class="form-control" name="address_home" rows="3" placeholder="Direccion (Casa)"></textarea>
           </div>
@@ -150,12 +164,10 @@
             <textarea class="form-control" name="address_work" rows="3" placeholder="Direccion (Trabajo)"></textarea>
           </div>
 
-          <div class="col-md-6 mt-3">
-            <div class="row">
-              <div class="col-6 col-md-4">
-                <label class="px-2">Zona de Cobro:</label>
-              </div>
-              <div class="col-6 col-md-8">
+          <div class="col-md-12 mt-3 text-center">
+            <label class="px-2">Zona de Cobro:</label>
+            <div class="row justify-content-center">
+              <div class="col-8 col-md-4">
                 <select class="form-control custom-select" name="zone_id">
                   @foreach ( $zones as $zone)
                   <option value="{{ $zone->id }}">
@@ -165,14 +177,9 @@
                 </select>
               </div>
             </div>
-            
-
-
-          </div>
-
+          </div><!-- /col-6 -->
         </div>
         @endif
-
       </div>
 
       <div class="col-12 text-center py-5">
@@ -187,80 +194,117 @@
 
 @section('scripts')
 <script type="text/javascript">
-
-  // initialize the sliders
-  var interest_slider = document.getElementById('interest_slider');
-  var duration_slider = document.getElementById('duration_slider');
-  
-  noUiSlider.create(interest_slider, {start: 20, step: 10, range: { 'min': 0, 'max': 100 } });
-  noUiSlider.create(duration_slider, {start: 6, step: 1, range: { 'min': 1, 'max': 12 } });
-  
-  // Event binding
-  interest_slider.noUiSlider.on('update', onInterestSliderUpdate);
-  duration_slider.noUiSlider.on('update', onDurationSliderUpdate);
-  
-  $('#input_amount').on('input', onAmountValueChange);
-  
-  
-  get('input_amount').value = nicecify( valueOf('input_amount') );
-  
-  
+  /* Event binding */
+  $('#input_loan_amount').on('input', onLoanAmountChange);
+  $('#input_balance_amount').on('input', onBalanceAmountChange);
+  $('#input_duration').on('input', onDurationChange);
+  $('#input_mindue_amount').on('input', onMinimunDueChange);
   /* ------------------------------ Function Definitions ------------------------------ */
-  
-  function onInterestSliderUpdate(values)
+
+  function onLoanAmountChange()
   {
-    var int_rate = parseInt( values[0] );
-    get('label_interests').innerHTML =  int_rate;
-    get('interest').value = int_rate;
-    onAmountValueChange();
+   /* Get the value */
+   var d = get('input_loan_amount').value;
+   /* Check for a valid number */
+   let input_loan_amount =  ( d ) ? parseFloat(d.replaceAll(',', '')) : 0;
+   /* Update the Form */    
+   get('loaned').value = input_loan_amount;
+   /* Update all the labels */
+   window.update();
+ }
+
+ function onBalanceAmountChange()
+ {
+   /* Get the value */
+   var d = get('input_balance_amount').value;
+   /* Check for a valid number */
+   let input_balance_amount =  ( d ) ? parseFloat(d.replaceAll(',', '')) : 0;
+   /* Update the Form */    
+   get('balance').value = input_balance_amount;
+   /* Update all the labels */
+   window.update();
+ }
+
+ function onDurationChange()
+ {
+  /* Get the value */
+  var input_duration = get('input_duration').value;
+  /* Update the Form */    
+  get('duration').value = input_duration;
+  /* Update all the labels */
+  window.update();
+}
+
+function onMinimunDueChange()
+{
+  let intval = get('intval').value;
+
+  /* Get the value */
+  var d = get('input_mindue_amount').value;
+  /* Check for a valid number */
+  let mindue = ( d ) ? parseFloat(d.replaceAll(',', '')) : 0;
+
+  /* Update the Form */
+  get('mindue').value = mindue;
+  
+
+  /* Update the excedent label */
+  get('label_extmin').innerHTML = ( mindue - intval );
+  get('label_extmin').style.color = ((mindue - intval) < 0) ? 'red' : 'black';
+
+
+
+}
+
+function update()
+{
+  let loan_amount = get('loaned').value;
+  let balance_amount = get('balance').value;
+  let duration = get('duration').value;
+
+  let regdue = 0;
+  let mindue = 0;
+  let intval = 0;
+  let intrate = 0;
+
+  if ( loan_amount > 0 && balance_amount > 0 && duration > 0)
+  {
+    /* Calc the interest rate */
+    intrate = ( (balance_amount - loan_amount) / loan_amount );
+    /* Calc the due and minimin due */
+    var normdue = ( loan_amount / duration ).toFixed(0);
+    var intdue = ( normdue * intrate ).toFixed(0);
+    /* Set the values on display */
+    regdue = ( parseInt(normdue) + parseInt(intdue) );
+    intval = mindue = intdue;
   }
-  
-  function onDurationSliderUpdate(values)
-  {
-    var duration = parseInt( values[0] );
-    get('label_duration').innerHTML =  duration;
-    // get('wk_label_duration').innerHTML =  duration;
-    // get('bw_label_duration').innerHTML =  duration;
-    // get('mo_label_duration').innerHTML =  duration;
-    get('duration').value = duration;
-    
-    onPayPlanChange( get('payplan').value );
-    
-    
-  }
-  
-  function onAmountValueChange()
-  {
-    var int_rate= get('interest').value;
-    
-    var input_amount = get('input_amount').value;
-    
-    // Check for a valid number
-    input_amount =  ( input_amount ) ? parseFloat(input_amount.replaceAll(',', '')) : 0;
-    
-    var total_amount = input_amount + ( ( input_amount * int_rate ) / 100 );
-    
-    // Update the Form
-    get('loan').value = input_amount;
-    get('total').value = total_amount;
-    
-    // Update the options
-    window.updatePayPlanOptions()
-    
-    if (input_amount > 0)
-    {
-      get('label_loan_amount').innerHTML = nicecify( total_amount );
-    }
-    else
-    {
-      get('label_loan_amount').innerHTML = 0;
-    }
-    
-  }
-  
-  
-  function onPayPlanChange( plan )
-  {
+
+  /* Update the Form */
+  get('intrate').value = intrate *100;
+  get('intval').value = intval;
+  get('regdue').value = regdue;
+  get('mindue').value = mindue;
+
+  /* Update the interests label */
+  get('label_interests').innerHTML = ( intrate *100 ).toFixed(0);
+  get('label_interests').style.color = (intrate < 0) ? 'red' : 'black';
+
+  /* Update the dues ammount inputs */
+  get('input_regdue_amount').value = nicecify( regdue );
+  get('input_mindue_amount').value = nicecify( mindue );
+}
+
+
+function onDurationSliderUpdate(values)
+{
+  var duration = parseInt( values[0] );
+  get('label_duration').innerHTML =  duration;
+  get('duration').value = duration;
+  onPayPlanChange( get('payplan').value );
+}
+
+function onPayPlanChange( plan )
+{
     // get('payplan-we').classList.remove('active');
     // get('payplan-bw').classList.remove('active');
     // get('payplan-mo').classList.remove('active');
@@ -276,8 +320,8 @@
     // Set the value to the input
     get('payplan').value = plan;
     
-    // Update the options
-    window.updatePayPlanOptions()
+    /* Update the options */
+    // window.updatePayPlanOptions()
   }
   
   function updatePayPlanOptions()
@@ -304,10 +348,8 @@
     get('partial').value = dues_interest;
   }
   
-  
   function updatePayPlanDetails( plan )
   {
-
     var payday = get( plan + '_payday').value;
     get('details').value = payday;
   }
